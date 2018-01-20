fb0ccb2e887b6203a1c4a0d84b7913a91bf01347
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-01-20 22:39:06 +0100

    Update install script.

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,3 +1,7 @@
 rm ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 ln -s $(pwd)/Preferences.sublime-settings \
       ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+
+rm ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
+ln -s $(pwd)/Package\ Control.sublime-settings \
+      ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
