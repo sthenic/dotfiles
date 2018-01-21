c5978f918335a7345d4c42d525e3dfc877673ec7
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-01-21 17:05:08 +0100

    Added sublime keymap for Linux.

--- sublime/install.sh
+++ sublime/install.sh
@@ -5,3 +5,7 @@ ln -s $(pwd)/Preferences.sublime-settings \
 rm ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
 ln -s $(pwd)/Package\ Control.sublime-settings \
       ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
+
+rm ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap
+ln -s $(pwd)/Default\ \(Linux\).sublime-keymap \
+      ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap
