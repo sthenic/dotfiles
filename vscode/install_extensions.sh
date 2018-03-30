0ccc72273e02f0b4238161ff9edc7ab7226057fa
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-03-30 22:34:02 +0200

    vscode: Update extensions.

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -1,6 +1,8 @@
 code --install-extension Equinusocio.vsc-material-theme
 code --install-extension James-Yu.latex-workshop
+code --install-extension lextudio.restructuredtext
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension sadesyllas.vscode-workspace-switcher
+code --install-extension stkb.rewrap
 code --install-extension trixnz.vscode-lua
