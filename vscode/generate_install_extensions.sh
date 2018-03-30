36246709b4b81931cd4576d32370a18f16fe7d5a
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-03-30 21:55:07 +0200

    vscode: Generate extension install script for Windows.

--- vscode/generate_install_extensions.sh
+++ vscode/generate_install_extensions.sh
@@ -1,10 +1,13 @@
-FILENAME='install_extensions.sh'
+FILENAME_LINUX='install_extensions.sh'
+FILENAME_WINDOWS='install_extensions.bat'
 EXTENSIONS=$(code --list-extensions)
 
-# Truncate the file.
-: > $FILENAME
+# Truncate the files
+: > $FILENAME_LINUX
+: > $FILENAME_WINDOWS
 
 for EXT in $EXTENSIONS
 do
-    echo 'code --install-extension' $EXT >> $FILENAME
+    echo 'code --install-extension' $EXT >> $FILENAME_LINUX
+    echo 'call code --install-extension' $EXT >> $FILENAME_WINDOWS
 done
---
+++ vscode/install_extensions.bat
@@ -0,0 +1,6 @@
+call code --install-extension Equinusocio.vsc-material-theme
+call code --install-extension James-Yu.latex-workshop
+call code --install-extension ms-python.python
+call code --install-extension ms-vscode.cpptools
+call code --install-extension sadesyllas.vscode-workspace-switcher
+call code --install-extension trixnz.vscode-lua
