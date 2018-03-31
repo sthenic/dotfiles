c7cf496d6499778cb126f99e30a23f75e8f1fc0f
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-03-31 21:31:28 +0200

    vscode: Update settings.

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -4,5 +4,6 @@ code --install-extension lextudio.restructuredtext
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension sadesyllas.vscode-workspace-switcher
+code --install-extension shuworks.vscode-table-formatter
 code --install-extension stkb.rewrap
 code --install-extension trixnz.vscode-lua
